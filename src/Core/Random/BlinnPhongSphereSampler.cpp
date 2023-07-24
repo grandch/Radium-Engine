f2e323c69af121dc820a056fd4a4c257d815da7e
grandch <charliegrand31@gmail.com>
2023-07-24 18:17:49 +0200

    fix blinnphong sampling

--- src/Core/Random/BlinnPhongSphereSampler.cpp
+++ src/Core/Random/BlinnPhongSphereSampler.cpp
@@ -19,15 +19,15 @@ Ra::Core::Random::BlinnPhongSphereSampler::getDir( UniformGenerator* generator, 
     Vector3 dir;
     Vector2 u = generator->get2D();
 
-    Scalar cosTheta = std::cos( std::pow( u[0], 1_ra / ( roughness + 2 ) ) );
-    Scalar sinTheta = std::cos( Math::Pi / 2_ra - std::pow( u[0], 1_ra / ( roughness + 2 ) ) );
+    Scalar cosTheta = std::pow( 1_ra - u[0], 1_ra / ( roughness + 2 ) );
+    Scalar sinTheta = std::sqrt( 1_ra - cosTheta * cosTheta );
     Scalar phi      = 2 * Math::Pi * u[1];
 
     dir[0] = sinTheta * std::cos( phi );
     dir[1] = sinTheta * std::sin( phi );
     dir[2] = cosTheta;
 
-    return { dir, ( roughness + 2 ) * std::pow( u[0], roughness ) / ( 2 * Math::Pi ) };
+    return { dir, ( roughness + 2 ) * std::pow( 1_ra - u[0], roughness ) / ( 2 * Math::Pi ) };
 }
 
 Scalar BlinnPhongSphereSampler::pdf( Vector3 dir, Vector3 normal, Scalar roughness ) {
